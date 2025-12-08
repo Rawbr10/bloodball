1f66745b33d68adc6e696c88b176840693f50aaa
Roblox Scripts <gregyheport@gmail.com>
2025-12-08 19:42:42 +0800

    Update loader.js

--- api/loader.js
+++ api/loader.js
@@ -11,7 +11,34 @@ export default function handler(req, res) {
     const Rrl = "https://github.com/Rawbr10/loader/raw/refs/heads/main/ProjectX-Steal-A-Brainrot-Anti-Loader.lua";
     const payload = Buffer.from(Rrl).toString('base64');
 
-    const script = `loadstring(game:HttpGet(game:GetService("HttpService"):UrlDecode("${payload}")))()`;
+    const script = `
+local b="${payload}"
+local url=""
+for i=1,#b,1 do
+    local c=b:sub(i,i)
+    if c~"+" and c~"=" and c~"/" then
+        url=url..c
+    end
+end
+loadstring(game:HttpGet((function(s)
+    local t={}
+    for i=1,#s,4 do
+        local a=s:sub(i,i+3)
+        local n=0
+        for j=1,4 do
+            local v=(a:sub(j,j))
+            if v>="A" and v<="Z" then n=n*64+(v:byte()-65)
+            elseif v>="a" and v<="z" then n=n*64+(v:byte()-71)
+            elseif v>="0" and v<="9" then n=n*64+(v:byte()-48)
+            elseif v=="+" then n=n*64+62
+            elseif v=="/" then n=n*64+63
+            end
+        end
+        t[#t+1]=string.char(bit32.rshift(n,16),bit32.band(bit32.rshift(n,8),255),bit32.band(n,255))
+    end
+    return table.concat(t):gsub("%z+$","")
+end)(url)))()
+`;
 
     res.status(200).send(script);
 }
